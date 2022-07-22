5f3f6ec51efb9f46be7e1df45694d0d68a4615e7
Joseph B <100744018+jobloom79@users.noreply.github.com>
2022-07-21 22:29:59 -0400

    9.4.3

--- Resources/app.py
+++ Resources/app.py
@@ -1,9 +1,5 @@
-from cmath import exp
 from flask import Flask
-from sklearn.tree import export_graphviz
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello world!!!'
-
-export FLASK_APP = app.py
+    return 'Hello World'
